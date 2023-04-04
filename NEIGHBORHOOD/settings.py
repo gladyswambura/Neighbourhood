f2073d01f0e506c68572c10d4ab3a8a29d92354c
gladys <gladyswahito7@gmail.com>
2023-04-04 08:18:30 +0300

    redeploy

--- NEIGHBORHOOD/settings.py
+++ NEIGHBORHOOD/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    ''
+    'https://web-production-f298.up.railway.app'
 ]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
