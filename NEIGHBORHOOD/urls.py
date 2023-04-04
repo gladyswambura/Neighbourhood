91ca221eb8b6b377a8dfdcd0630ce53e2b9986a1
gladys <gladyswahito7@gmail.com>
2023-04-04 08:09:05 +0300

    redeploy

--- NEIGHBORHOOD/urls.py
+++ NEIGHBORHOOD/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for project project.
+"""NEIGHBORHOOD URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -27,3 +27,4 @@ urlpatterns = [
 ]  
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
