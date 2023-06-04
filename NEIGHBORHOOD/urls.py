0c5e8d4b525059c2bc5695253410b294e20f36dc
gladys <gladyswahito7@gmail.com>
2023-06-04 18:17:51 +0300

    migrating the database

--- NEIGHBORHOOD/urls.py
+++ NEIGHBORHOOD/urls.py
@@ -26,7 +26,5 @@ urlpatterns = [
     path('', include ('neighborhood.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]  
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns+= staticfiles_urlpatterns()
